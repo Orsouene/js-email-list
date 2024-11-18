135f524b647f02cc82320a0defe620f1d165f7b8
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-18 17:08:53 +0100

    Fatto il bonus

--- script/script.js
+++ script/script.js
@@ -7,16 +7,45 @@ const button = document.getElementById("button");
 
 // https://flynn.boolean.careers/exercises/api/random/mail => (ritorna una email)
 
+//////////////////////////////-B.O.N.U.S-//////////
+
+button.addEventListener("click", genera);
+
+//  un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+
+function genera() {
+  let template = "";
+  for (let i = 0; i < 10; i++) {
+    // La richiesta fatta con Axios per ottenere i dati dall'URL
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((res) => {
+        console.log(res.data.response);
+        template += `<li> ${res.data.response}</li> <hr style="margin:0px;">`;
+        uListe.innerHTML = template;
+      })
+      // Se c'è un errore nel codice
+      .catch((error) => {
+        console.log(error);
+      })
+      //   Messaggio finale che verrà stampato indipendentemente dal risultato
+      .finally(() => {
+        console.log("Mail Generato");
+      });
+  }
+}
+
+//////////////////////////////-ESERCIZIO-//////////
 //  / Versione per generare 10 indirizzi email e stamparli in pagina all'interno di una lista
 
-for (let i = 0; i < 10; i++) {
-  axios
-    .get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((res) => {
-      console.log(res.data.response);
-      uListe.innerHTML += `<li> ${res.data.response}</li> <hr>`;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+// for (let i = 0; i < 10; i++) {
+//   axios
+//     .get("https://flynn.boolean.careers/exercises/api/random/mail")
+//     .then((res) => {
+//       console.log(res.data.response);
+//       uListe.innerHTML += `<li> ${res.data.response}</li> <hr>`;
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
